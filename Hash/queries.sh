a7fdc86ec693db644f100de67a05b98c66ac6fc9
Joseph Garner <s3550293@student.rmit.edu.au>
2018-05-15 15:30:56 +1000

    Fixed Queries

--- Hash/queries.sh
+++ Hash/queries.sh
@@ -1,4 +1,4 @@
-java hashquery "Warby Wares" 4096
+java hashquery "FOOD NOT FADS" 4096
 java hashquery "SWORDFISH CONSTRUCTIONS" 4096
 java hashquery "TRINITY REHAB" 4096
 java hashquery "Willandra Rowland Flat" 4096
