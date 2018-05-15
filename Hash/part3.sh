2e38608ebf0610147ece3e1e8d62d4c11245fc65
Joseph Garner <s3550293@student.rmit.edu.au>
2018-05-15 15:18:22 +1000

    Added query files for linux for testig

--- Hash/part3.sh
+++ Hash/part3.sh
@@ -7,9 +7,7 @@ then
 	echo "compiling code"
 	javac *.java
 	echo -e "loading heap file\n"
-	java dbload -p 4096 BUSINESS_NAMES_201803.csv.nohead
-	echo "querying heap for \"mf engineering\""
-	java dbquery "mf engineering" 4096
+	java hashload -p 4096 BUSINESS_NAMES_201803.csv.nohead
 else
 	echo "Usage: bash ./part3.sh input_csv_file"
 fi
